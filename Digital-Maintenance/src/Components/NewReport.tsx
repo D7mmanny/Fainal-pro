4d26c0fe7449eff245429f33fca79e10b22a362d
shazmend <engshathaalfqih@gmail.com>
2023-09-07 17:54:27 +0300

    report

--- Digital-Maintenance/src/Components/NewReport.tsx
+++ Digital-Maintenance/src/Components/NewReport.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import axios from "axios";
 
 function NewReport() {
-  //   type IreportApi = {
-  //     supervisorName: string;
-  //     type: string;
-  //     factory: string;
-  //     report: string;
-  //   };
 
   //state
   const [report, setReport] = React.useState("");
@@ -27,29 +21,31 @@ function NewReport() {
       })
       .then((response) => {
         console.log(response);
+        window.location.reload()
       });
+    
+
   };
   return (
-      <div className="w-7/12 ">
-        <div className=" bg-myGray border w-6/6 h-4/5 flex flex-col items-center gap-5 rounded-xl pb-6">
-          <input
-            value={`${factory} Factory`}
-            className="text-center py-1 px-8 rounded-xl mt-7 text-gray-700"
-          />
-          <textarea
-            className=" w-5/6 h-96 px-4"
-            placeholder="Write the report here..."
-            onChange={(e) => setReport(e.target.value)}
-          />
-          <button
-            className=" bg-light-blue py-1 px-7 rounded-xl text-white"
-            onClick={submitReport}
-          >
-            Submit
-          </button>
-        </div>
+    <div className="w-7/12 ">
+      <div className=" bg-myGray border w-6/6 h-4/5 flex flex-col items-center gap-5 rounded-xl pb-6">
+        <input
+          value={`${factory} Factory`}
+          className="text-center py-1 px-8 rounded-xl mt-7 text-gray-700"
+        />
+        <textarea
+          className=" w-5/6 h-96 px-4"
+          placeholder="Write the report here..."
+          onChange={(e) => setReport(e.target.value)}
+        />
+        <button
+          className=" bg-light-blue py-1 px-7 rounded-xl text-white"
+          onClick={submitReport}
+        >
+          Submit
+        </button>
       </div>
+    </div>
   );
 }
-
 export default NewReport;
