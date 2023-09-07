43d70781ad3b1bd8260a93ef126d4f01ae7382ca
D7mmanny <d7mmanny@gmail.com>
2023-09-07 11:22:22 +0300

    AddButtonColorToTailwind

--- Digital-Maintenance/src/Pages/HomeSupervisor.tsx
+++ Digital-Maintenance/src/Pages/HomeSupervisor.tsx
@@ -11,17 +11,20 @@ function HomeSupervisor() {
         {/* Supervisor Panel */}
         <div className='flex flex-col items-center gap-10 '>
         <h1 className='w-7/12'>Supervisor Panel</h1>
-            <div className='w-8/12 h-96 flex justify-evenly shadow-2xl'>  
-                <div>
-                    new task
+            <div className='w-8/12 h-96 flex justify-evenly shadow-2xl text-xl '>  
+                <div className='w-2/12'>
+                    <button className='w-full h-14  bg-slate-300 rounded-full shadow-lg bg-button'>new task +</button>
+                    
                 </div>
                 
-                <div>
-                    new report
+                <div className='w-2/12'>
+                    <button className=' w-full h-14 bg-slate-300 rounded-full shadow-lg bg-button'>new report +</button>
+                    
                 </div>
 
-                <div>
-                    Shutdown
+                <div className='w-2/12'>
+                    <button className='w-full h-14 bg-slate-300 rounded-full shadow-lg bg-button'> Shutdown</button>
+                   
                 </div>
             </div>
         </div>
--- Digital-Maintenance/tailwind.config.js
+++ Digital-Maintenance/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {},
+    colors:{
+      button:'#E8EEEF',
+    },
   },
   plugins: [],
 }
