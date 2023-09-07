db9804c4eb4021325ebeb5149764c38bcf12822a
D7mmanny <d7mmanny@gmail.com>
2023-09-07 18:41:39 +0300

    a

--- Digital-Maintenance/src/Components/TechnicianName.tsx
+++ Digital-Maintenance/src/Components/TechnicianName.tsx
@@ -7,7 +7,8 @@ function TechnicianName(props:TechnicianName) {
         <div className="w-80 ">
             <div className="bg-myGray w-96 h-14 flex rounded-2xl">
             <img className="p-2" src="https://img.freepik.com/premium-vector/gray-avatar-icon-vector-illustration_276184-163.jpg?w=2000" alt="a" />
-            <h1>{props.name}</h1>
+            <a href="">{props.name}</a>
+           
             </div>
             
         </div>
