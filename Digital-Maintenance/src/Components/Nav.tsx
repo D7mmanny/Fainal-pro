d82a3fd07bae433d3fd582e0553f48ddbbd90fd0
shazmend <engshathaalfqih@gmail.com>
2023-09-07 11:47:21 +0300

    nav

---
+++ Digital-Maintenance/src/Components/Nav.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import WhiteLogo from '../assets/WhiteLogo.svg'
+
+function Nav() {
+  return (
+    <div>
+        <nav className=' bg-dark-blue'>
+            <div>
+                <img src={WhiteLogo} />
+            </div>
+            <ul>
+
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
