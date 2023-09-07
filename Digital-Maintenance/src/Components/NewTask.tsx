f37f9e84f769f5ae34165798e8ed47273eca7dbe
D7mmanny <d7mmanny@gmail.com>
2023-09-07 16:21:55 +0300

    test

--- Digital-Maintenance/src/Components/NewTask.tsx
+++ Digital-Maintenance/src/Components/NewTask.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 function NewTask() {
   return (
@@ -10,15 +9,17 @@ function NewTask() {
 
         <div className='mt-10'>
           <h1 className='text-center text-2xl p-10'>Chose the Lab</h1>
+          
           <div className='flex justify-center '>
-            <ul className='flex gap-5 text-xl w-4/5 border-solid border-4 p-5 rounded-xl'>
-            <a className='bg-light-blue w-2/12 h-14 text-white rounded-xl flex justify-center items-center'  >Lab A2</a>
-            <a className='bg-light-blue w-2/12 text-white rounded-xl flex justify-center items-center' > Lab S8</a>
-            <a className='bg-light-blue w-2/12 text-white rounded-xl flex justify-center items-center' >Lab G5</a>
-            <a className='bg-light-blue w-2/12 text-white rounded-xl flex justify-center items-center' >Lab T3</a>
-            <a className='bg-light-blue w-2/12 text-white rounded-xl flex justify-center items-center' >Lab W4</a>
-           
-            </ul>
+
+            <select className='w-44 ' name="" id="" size={5}>
+              <option className='bg-light-blue  h-14 text-white rounded-xl flex justify-center items-center m-2' value="Lab A2">Lab A2</option>
+              <option className='bg-light-blue  h-14 text-white rounded-xl flex justify-center items-center m-2' value="Lab S8">Lab S8</option>
+              <option className='bg-light-blue  h-14 text-white rounded-xl flex justify-center items-center m-2' value="Lab G5">Lab G5</option>
+              <option className='bg-light-blue  h-14 text-white rounded-xl flex justify-center items-center m-2' value="Lab T3">Lab T3</option>
+              <option className='bg-light-blue  h-14 text-white rounded-xl flex justify-center items-center m-2' value="Lab W4">Lab W4</option>
+            </select>
+          
           </div>
         </div>
     </div>
