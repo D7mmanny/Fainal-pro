203d02603ec39f9568d279db80cdbaa0a665b94a
D7mmanny <d7mmanny@gmail.com>
2023-09-07 16:03:34 +0300

    tst

--- Digital-Maintenance/src/Components/NewTask.tsx
+++ Digital-Maintenance/src/Components/NewTask.tsx
@@ -7,6 +7,20 @@ function NewTask() {
             <a className=' bg-myGray w-1/2 h-full  flex justify-center items-center rounded-l-xl' href="">Inspection</a>
             <a className='bg-myGray w-1/2 h-full text-center flex justify-center items-center rounded-r-xl' href="">Maintenance</a>
         </div>
+
+        <div className='mt-10'>
+          <h1 className='text-center text-2xl p-10'>Chose the Lab</h1>
+          <div className='flex justify-center '>
+            <ul className='flex gap-5 text-xl w-4/5 border-solid border-4 p-5 rounded-xl'>
+            <a className='bg-light-blue w-2/12 h-14 text-white rounded-xl flex justify-center items-center'  >Lab A2</a>
+            <a className='bg-light-blue w-2/12 text-white rounded-xl flex justify-center items-center' > Lab S8</a>
+            <a className='bg-light-blue w-2/12 text-white rounded-xl flex justify-center items-center' >Lab G5</a>
+            <a className='bg-light-blue w-2/12 text-white rounded-xl flex justify-center items-center' >Lab T3</a>
+            <a className='bg-light-blue w-2/12 text-white rounded-xl flex justify-center items-center' >Lab W4</a>
+            <input type="text"  value={}/>
+            </ul>
+          </div>
+        </div>
     </div>
   )
 }
