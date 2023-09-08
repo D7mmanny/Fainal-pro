b45b53a3054a57251a5f43bf0e42e7b86ab3c7a4
D7mmanny <d7mmanny@gmail.com>
2023-09-08 19:33:09 +0300

    TechnicianInspectionReport

--- Digital-Maintenance/src/Pages/HomeTechnican.tsx
+++ Digital-Maintenance/src/Pages/HomeTechnican.tsx
@@ -3,6 +3,7 @@ import Nav from '../Components/Nav'
 
 //compoonants
 import Tasks from '../Components/Tasks'
+import TechnicianInspection from '../Components/TechnicianInspection'
 
 function HomeTechnican() {
 
@@ -13,9 +14,9 @@ function HomeTechnican() {
         {/* Supervisor Panel */}
         <div className='flex flex-col items-start gap-10 ml-40'>
         <h1 className='w-7/12 text-2xl mt-14'>Supervisor Panel</h1>
-          <div className='flex'>
+          <div className='flex w-full gap-5'>
             <Tasks/>
-            
+            <TechnicianInspection/>
           </div>
         </div>
     </div>
