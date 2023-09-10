4c1f475b9b0bdc72ac88e5557bd7ed2e22211bed
shazmend <engshathaalfqih@gmail.com>
2023-09-10 13:06:00 +0300

    task (done)

--- Digital-Maintenance/src/Components/TaskProps.tsx
+++ Digital-Maintenance/src/Components/TaskProps.tsx
@@ -6,12 +6,13 @@ interface Itask {
   MaintenanceType: string;
   Factory:string;
   Lab:string
+  OperationStatus:string;
 }
 
 function TaskProps(props: Itask) {
   return (
     <div>
-      {props.OperationType == "Inspection" && (
+      {props.OperationType == "Inspection" && props.OperationStatus!="Complete" && (
         <div className=" flex bg-InspectLightYelow w-96 rounded-xl justify-start items-center gap-2">
           <div className=" py-14 px-1 bg-InspectDarkYelow" />
           <div className="flex flex-col">
@@ -27,7 +28,7 @@ function TaskProps(props: Itask) {
           </div>
         </div>
       )}
-      {props.OperationType == "Maintanance" && (
+      {props.OperationType == "Maintanance" && props.OperationStatus!="Complete" && (
         <div className=" flex bg-MaintananceLightGreen w-96 rounded-xl justify-start items-center gap-2">
           <div className=" py-14 px-1 bg-MaintananceDarkGreen" />
           <div className="flex flex-col">
@@ -35,6 +36,25 @@ function TaskProps(props: Itask) {
             <li className="text-xl ">{props.OperationType}</li>
             <div className="flex gap-32 ml-7 mt-3 ">
               <p className="text-xs text-center"> Equipment:  <br/>  {props.Equipment}</p>
+              <p className="text-xs text-center">
+                {" "}
+                Maintenance Type: <br/> {props.MaintenanceType}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+      { props.OperationStatus=="Complete" && (
+        <div className=" flex bg-gray-200 w-96 rounded-xl justify-start items-center gap-2">
+          <div className=" py-14 px-1 bg-gray-400" />
+          <div className="flex flex-col">
+            <div className="flex text-xs justify-between">
+            <p className="text-xs ml-7 mb-3"> 2023/9/08</p>
+            <p className=" flex done">Done</p>
+            </div>
+            <li className="text-xl ">{props.OperationType}</li>
+            <div className="flex gap-32 ml-7 mt-3 ">
+              <p className="text-xs text-center"> Equipment:<br/>  {props.Equipment}</p>
               <p className="text-xs text-center">
                 {" "}
                 Maintenance Type: <br/> {props.MaintenanceType}
--- Digital-Maintenance/src/Components/Tasks.tsx
+++ Digital-Maintenance/src/Components/Tasks.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import TaskProps from "./TaskProps";
 import axios from "axios";
-import TechnicianInspection from '../Components/TechnicianInspection'
+import TechnicianInspection from "../Components/TechnicianInspection";
 import TechnicianMaintuinance from "./TechnicianMaintuinance";
-
 
 function Tasks() {
   type task = {
-    id:string,
+    id: string;
     OperationType: string;
     Equipment: string;
     MaintenanceType: string;
-    Factory:string;
-    Lab:string;
-    Technician:string;
-    OperationStatus:string;
+    Factory: string;
+    Lab: string;
+    Technician: string;
+    OperationStatus: string;
   };
 
   //localsrorage
@@ -23,7 +22,9 @@ function Tasks() {
   //state
   const [taskApi, setTaskApi] = React.useState<task[]>([]);
   const [taskDetails, setTaskDetails] = React.useState("Inspection");
-  const [taskTitle , setTaskTaitle] = React.useState(localStorage.getItem("taskLab"));
+  const [taskTitle, setTaskTaitle] = React.useState(
+    localStorage.getItem("taskLab")
+  );
 
   //get Task Api info
   React.useEffect(() => {
@@ -35,45 +36,48 @@ function Tasks() {
   }, [taskTitle]);
 
   return (
-    <div className="flex gap-24 w-full " >
+    <div className="flex gap-24 w-full ">
       <div className="h-full">
-      <div className=" Tasks flex flex-col gap-4 py-5 bg-myGray drop-shadow-sm px-4 rounded-xl mb-5 overflow-auto">
-        {taskApi.map((task) => {
-          if(task.Technician == name){
-            return(
-              <a
-            className="  cursor-pointer"
-            onClick={() => {
-              setTaskDetails(task.OperationType)
-              localStorage.setItem("taskDetails", task.OperationType);
-              localStorage.setItem("taskId",task.id)
-              localStorage.setItem("taskLab",task.Lab)
-              setTaskTaitle(task.Lab)
-              localStorage.setItem("Equipment",task.Equipment)
-              localStorage.setItem("MaintenanceType",task.MaintenanceType)
-            }}
-          >
-            
-            <TaskProps
-              OperationType={task.OperationType}
-              Equipment={task.Equipment}
-              MaintenanceType={task.MaintenanceType}
-              Factory={task.Factory}
-              Lab={task.Lab}
-            />
-          </a>
-            )
-         }
-        }
-          
-        )}
-      </div>
+        <div className=" Tasks flex flex-col gap-4 py-5 bg-myGray drop-shadow-sm px-4 rounded-xl mb-5 overflow-auto">
+          {taskApi.map((task) => {
+            if (task.Technician == name) {
+              return (
+                <a
+                  className="  cursor-pointer"
+                  onClick={() => {
+                    if (task.OperationStatus != "Complete") {
+                      setTaskDetails(task.OperationType);
+                      localStorage.setItem("taskDetails", task.OperationType);
+                      localStorage.setItem("taskId", task.id);
+                      localStorage.setItem("taskLab", task.Lab);
+                      setTaskTaitle(task.Lab);
+                      localStorage.setItem("Equipment", task.Equipment);
+                      localStorage.setItem(
+                        "MaintenanceType",
+                        task.MaintenanceType);  
+                    } else{
+                      localStorage.setItem("taskDetails" , "null")
+                    }
+                  }}
+                >
+                  <TaskProps
+                    OperationType={task.OperationType}
+                    Equipment={task.Equipment}
+                    MaintenanceType={task.MaintenanceType}
+                    Factory={task.Factory}
+                    Lab={task.Lab}
+                    OperationStatus={task.OperationStatus}
+                  />
+                </a>
+              );
+            }
+          })}
+        </div>
       </div>
       <div className="w-full h-full flex justify-center">
-        {taskDetails == "Inspection" && <TechnicianInspection/>}
-        {taskDetails == "Maintanance" && <TechnicianMaintuinance/>}
+        {taskDetails == "Inspection" && <TechnicianInspection />}
+        {taskDetails == "Maintanance" && <TechnicianMaintuinance />}
       </div>
-     
     </div>
   );
 }
