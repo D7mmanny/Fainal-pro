1941bf182261d19c7c50440d4dbf18bde2625d3f
shazmend <engshathaalfqih@gmail.com>
2023-09-11 01:50:03 +0300

    cityFactoryReport

---
+++ Digital-Maintenance/src/Components/CityFactoryReports.tsx
@@ -0,0 +1,131 @@
+import React from "react";
+import axios from "axios";
+import Nav from "../Components/Nav";
+import user from '../assets/user.svg'
+import { useNavigate } from "react-router-dom";
+
+function CityFactoryReports() {
+    const nav = useNavigate();
+  const CityFactoryReports = localStorage.getItem("CityFactoryReports");
+  type task = {
+    id: string;
+    supervisorName: string;
+    type: string;
+    factory: string;
+    report: string;
+  };
+  const [taskApi, setTaskApi] = React.useState<task[]>([]);
+
+  React.useEffect(() => {
+    axios
+      .get("https://64f8e5cc824680fd21802e48.mockapi.io/FactoryReport")
+      .then((response) => {
+        setTaskApi(response.data);
+      });
+  }, []);
+  return (
+    <div>
+      <div>
+        <Nav />
+      </div>
+
+      <section className="container mx-auto p-6 font-mono">
+        <div className="w-full flex justify-between items-center py-5">
+          <h1 className="pl-3 text-2xl ">Factory Reports</h1>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            className="w-7 h-7 mr-4 cursor-pointer"
+            onClick={() => {
+              nav("/HomeManager");
+            }}
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </div>
+
+        <div className="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+          <div className="w-full overflow-x-auto">
+            <table className="w-full">
+              <thead>
+                <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+                  <th className="px-4 py-3">Name</th>
+                  <th className="px-4 py-3">Factory </th>
+                  <th className="px-4 py-3">Type</th>
+                  <th className="px-4 py-3">Report</th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {taskApi.map((task) => {
+                  {
+                    if ( task.factory == CityFactoryReports) {
+                      return (
+                        <tr className="text-gray-700">
+                          <td className="px-4 py-3 border">
+                            <div className="flex items-center text-sm">
+                              <div className="relative w-8 h-8 mr-3 rounded-full">
+                                <img
+                                  className="object-cover w-full h-full rounded-full"
+                                  src={user}
+                                  alt=""
+                                  loading="lazy"
+                                />
+                                <div
+                                  className="absolute inset-0 rounded-full shadow-inner"
+                                  aria-hidden="true"
+                                ></div>
+                              </div>
+                              <div>
+                                <p className="font-semibold text-black">
+                                  {task.supervisorName}
+                                </p>
+                                <p className="text-xs text-gray-600">
+                                  supervisor
+                                </p>
+                              </div>
+                            </div>
+                          </td>
+                          <td className="px-4 py-3 text-md font-semibold border">
+                            {task.factory} Factory{" "}
+                          </td>
+                          <td className="px-4 py-3 text-xs border">
+                            <div className="flex justify-center items-center">
+                              {task.type == "Monthly report" && (
+                                <h4 className="bg-green-100 text-green-600 rounded-full p-2 w-3/6 text-center">
+                                  Monthly report
+                                </h4>
+                              )}
+                              {task.type == "Shutdown" && (
+                                <h4 className="bg-red-100 text-red-600 rounded-full p-2 w-3/6 text-center">
+                                  Shutdown
+                                </h4>
+                              )}
+                            </div>
+                          </td>
+                          <td className="px-4 py-3 text-sm border">
+                            <a href={`/FactoryReports/${task.id}`}>
+                              {task.report}
+                            </a>
+                          </td>
+                        </tr>
+                      );
+                    }
+                  }
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default CityFactoryReports;
--- Digital-Maintenance/src/Pages/HomeManager.tsx
+++ Digital-Maintenance/src/Pages/HomeManager.tsx
@@ -1,125 +1,190 @@
 // import React from 'react'
-import Nav from '../Components/Nav'
-import Map from '../assets/Map.svg'
+import Nav from "../Components/Nav";
+import Map from "../assets/Map.svg";
+import { useNavigate } from "react-router-dom";
 
 // Saudi Map
-import one from '../assets/1.svg'
-import two from '../assets/2.svg'
-import three from '../assets/3.svg'
-import four from '../assets/4.svg'
-import five from '../assets/5.svg'
-import six from '../assets/6.svg'
-import seven from '../assets/7.svg'
-import eight from '../assets/8.svg'
-import nine from '../assets/9.svg'
-import ten from '../assets/10.svg'
-import Eleven from '../assets/11.svg'
-import Twelve from '../assets/12.svg'
-import thirteen from '../assets/13.svg'
+import one from "../assets/1.svg";
+import two from "../assets/2.svg";
+import three from "../assets/3.svg";
+import four from "../assets/4.svg";
+import five from "../assets/5.svg";
+import six from "../assets/6.svg";
+import seven from "../assets/7.svg";
+import eight from "../assets/8.svg";
+import nine from "../assets/9.svg";
+import ten from "../assets/10.svg";
+import Eleven from "../assets/11.svg";
+import Twelve from "../assets/12.svg";
+import thirteen from "../assets/13.svg";
 
 // Factory
-import Jeddah from '../assets/Jeddah.svg'
-import Najran from '../assets/Najran.svg'
-import Riyadh from '../assets/Riyadh.svg'
-import alkhubar from '../assets/alkhubar.svg'
-import north from '../assets/north.svg'
-import Tabuk from '../assets/Tabuk.svg'
-
- 
-
-
-
-
-
-
+import Jeddah from "../assets/Jeddah.svg";
+import Najran from "../assets/Najran.svg";
+import Riyadh from "../assets/Riyadh.svg";
+import alkhubar from "../assets/alkhubar.svg";
+import north from "../assets/north.svg";
+import Tabuk from "../assets/Tabuk.svg";
 
 function HomeManager() {
+  const nav = useNavigate();
   return (
     <div>
-        <Nav/>      
-        <div className='Home bg-dark-blue'>
-          {/* Map background */}
-          <img className='w-screen'
-           src={Map}/>
-           
-           {/*Saudi Map*/}
-           <div>
-            <img className='map1 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer ' 
-            src={one}/>
-             <img className=' map2  transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={two}/>
-             <img className=' map3 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={three}/>
-            <img className=' map4 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={four}/>
-             <img className=' map5 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={five}/>
-            <img className=' map6 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={six}/>
-             <img className=' map7 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={seven}/>
-             <img className=' map8 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={eight}/>
-             <img className=' map9 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={nine}/>
-             <img className=' map10 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={ten}/>
-            <img className=' map11  transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={Eleven}/>
-             <img className='map12 pl-2 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={Twelve}/>
-             <img className='map13 pl-2 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer' 
-            src={thirteen}/>
+      <Nav />
+      <div className="Home bg-dark-blue">
+        {/* Map background */}
+        <img className="w-screen" src={Map} />
 
-            {/* factory */}
+        {/*Saudi Map*/}
+        <div>
+          <img
+            className="map1 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer "
+            src={one}
+          />
+          <img
+            className=" map2  transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={two}
+          />
+          <img
+            className=" map3 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={three}
+          />
+          <img
+            className=" map4 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={four}
+          />
+          <img
+            className=" map5 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={five}
+          />
+          <img
+            className=" map6 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={six}
+          />
+          <img
+            className=" map7 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={seven}
+          />
+          <img
+            className=" map8 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={eight}
+          />
+          <img
+            className=" map9 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={nine}
+          />
+          <img
+            className=" map10 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={ten}
+          />
+          <img
+            className=" map11  transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={Eleven}
+          />
+          <img
+            className="map12 pl-2 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={Twelve}
+          />
+          <img
+            className="map13 pl-2 transition ease-in-out delay-150 hover:-translate-y hover:scale-105 cursor-pointer"
+            src={thirteen}
+          />
 
-            {/* jeddah */}
-            <div className='jeddah flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 '>
-            <img className=' transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer' 
-            src={Jeddah}/>
+          {/* factory */}
+
+          {/* jeddah */}
+          <div
+            onClick={() => {
+              localStorage.setItem("CityFactoryReports", "Jeddah");
+              nav("/CityFactoryReports");
+            }}
+            className="jeddah flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 "
+          >
+            <img
+              className=" transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer"
+              src={Jeddah}
+            />
             <p>Jeddah Factory</p>
-            </div>
-            
-            {/* Najran */}
-            <div className='Najran flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 '>
-            <img className=' transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer' 
-            src={Najran}/>
+          </div>
+
+          {/* Najran */}
+          <div
+            onClick={() => {
+              localStorage.setItem("CityFactoryReports", "Najran");
+              nav("/CityFactoryReports");
+            }}
+            className="Najran flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 "
+          >
+            <img
+              className=" transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer"
+              src={Najran}
+            />
             <p>Najran Factory</p>
-            </div>
+          </div>
 
-             {/* Riyadh */}
-             <div className='Riyadh flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 '>
-            <img className=' transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer' 
-            src={Riyadh}/>
+          {/* Riyadh */}
+          <div
+            onClick={() => {
+              localStorage.setItem("CityFactoryReports", "Riyadh");
+              nav("/CityFactoryReports");
+            }}
+            className="Riyadh flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 "
+          >
+            <img
+              className=" transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer"
+              src={Riyadh}
+            />
             <p>Riyadh Factory</p>
-            </div>
+          </div>
 
-            {/* alkhubar */}
-            <div className='alkhubar flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 '>
-            <img className=' transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer' 
-            src={alkhubar}/>
+          {/* alkhubar */}
+          <div
+            onClick={() => {
+              localStorage.setItem("CityFactoryReports", "Alkhubar");
+              nav("/CityFactoryReports");
+            }}
+            className="alkhubar flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 "
+          >
+            <img
+              className=" transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer"
+              src={alkhubar}
+            />
             <p>al-khubar Factory</p>
-            </div>
+          </div>
 
-            {/* north */}
-            <div className='north flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 '>
-            <img className=' transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer' 
-            src={north}/>
+          {/* north */}
+          <div
+            onClick={() => {
+              localStorage.setItem("CityFactoryReports", "North");
+              nav("/CityFactoryReports");
+            }}
+            className="north flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 "
+          >
+            <img
+              className=" transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer"
+              src={north}
+            />
             <p>North Factory</p>
-            </div>
+          </div>
 
-            {/* Tabuk */}
-            <div className='Tabuk flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 '>
-            <img className=' transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer' 
-            src={Tabuk}/>
+          {/* Tabuk */}
+          <div
+            onClick={() => {
+              localStorage.setItem("CityFactoryReports", "Tabuk");
+              nav("/CityFactoryReports");
+            }}
+            className="Tabuk flex items-center gap-1 text-transparent hover:text-white transition ease-in-out delay-300 "
+          >
+            <img
+              className=" transition ease-in-out delay-150 hover:-translate-y border-2 border-green-600 hover:border-white rounded-full cursor-pointer"
+              src={Tabuk}
+            />
             <p>Tabuk Factory</p>
-            </div>
-           
-
-           </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomeManager
+export default HomeManager;
