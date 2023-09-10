69e9a235e3f21955758bc44d0776f441cd8ba6fd
shazmend <engshathaalfqih@gmail.com>
2023-09-11 02:14:11 +0300

    TechnicanReport design

--- Digital-Maintenance/src/Pages/TechnicianReport.tsx
+++ Digital-Maintenance/src/Pages/TechnicianReport.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import AllReport from "../Components/AllReport";
+import user from '../assets/user.svg'
 import Nav from "../Components/Nav";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -28,61 +29,72 @@ function TechnicianReport() {
       <div>
         <Nav />
       </div>
-      <div className="p-10 h-screen">
-        <div className="flex justify-between items-center">
-          <h1 className="pl-3 text-4xl ">Labs reports</h1>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            className="w-7 h-7 mr-7 cursor-pointer"
-            onClick={()=>{nav("/HomeSup")}}
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </div>
-        <div className=" shadow-2xl w-12/12 h-5/6 ">
-          <div className=" m-5 h-5/6 border-solid border-2 border-gray-500 overflow-auto ">
-            <div className="border-solid border-b-2 flex border-gray-500 shadow-2xl sticky  top-0 ">
-              <h4 className="bg-gray-300 w-1/6 h-14 border-solid border-r-2 border-gray-500 flex justify-center items-center">
-                Technician name
-              </h4>
-              <h4 className="bg-gray-300 w-1/6 h-14 border-solid border-r-2 border-gray-500 flex justify-center items-center">
-                Status
-              </h4>
-              <h4 className="bg-gray-300 w-1/6 h-14 border-solid border-r-2 border-gray-500 flex justify-center items-center">
-                Lab
-              </h4>
-              <h4 className="bg-gray-300 w-1/6 h-14 border-solid border-r-2 border-gray-500 flex justify-center items-center">
-                Task type
-              </h4>
-              <h4 className="bg-gray-300 w-2/6 h-14 flex justify-center items-center">
-                Report
-              </h4>
-            </div>
-            <div>
-              {taskApi.map((task) => {
-                return (
-                  <AllReport
-                  id={task.id}
-                    Technician={task.Technician}
-                    OperationStatus={task.OperationStatus}
-                    Lab={task.Lab}
-                    OperationType={task.OperationType}
-                    report="show details"
-                  />
-                );
-              })}
-            </div>
+      <section className="container mx-auto p-6 font-mono">
+      <div className="w-full flex justify-between items-center py-5">
+            <h1 className="pl-3 text-2xl ">Factory Reports</h1>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              className="w-7 h-7 mr-4 cursor-pointer"
+              onClick={()=>{nav("/HomeManager")}}
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </div>
+
+        <div className="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+          <div className="w-full overflow-x-auto">
+            <table className="w-full">
+              <thead>
+                <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+                  <th className="px-4 py-3">Technican name</th>
+                  <th className="px-4 py-3">Lab</th>
+                  <th className="px-4 py-3">Task type</th>
+                  <th className="px-4 py-3">Status</th>
+                  <th className="px-4 py-3">Report</th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {taskApi.map((task) => {
+                  return (
+                    <tr className="text-gray-700">
+                    <td className="px-4 py-3 border">
+                      <div className="flex items-center text-sm">
+                        <div className="relative w-8 h-8 mr-3 rounded-full">
+                          <img className="object-cover w-full h-full rounded-full" src={user} alt="" loading="lazy" />
+                          <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
+                        </div>
+                        <div>
+                          <p className="font-semibold text-black">{task.Technician}</p>
+                          <p className="text-xs text-gray-600">Technician</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-4 py-3 text-md font-semibold border">Lab {task.Lab}</td>
+                    <td className="px-4 py-3 text-md font-semibold border">{task.OperationType}</td>
+                    <td className="px-4 py-3 text-xs border">
+                      <div className="flex justify-center items-center">
+                      {task.OperationStatus =="Complete" && <h4 className="bg-green-100 text-green-600 rounded-full p-2 w-3/6 text-center">Complete</h4>}
+                      {task.OperationStatus !="Complete" && <h4 className="bg-red-100 text-red-600 rounded-full p-2 w-3/6 text-center">InComplete</h4>} 
+                      </div>     
+                    </td>
+                    <td className="px-4 py-3 text-sm border"><a href={`/FactoryReports/${task.id}`}>{task.report}</a></td>
+                  </tr>
+                  );
+                })}
+                
+              </tbody>
+            </table>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
