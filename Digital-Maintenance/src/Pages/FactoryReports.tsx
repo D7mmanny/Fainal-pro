f6dab1dd2c656cf145a8d7f3e1bcd6c2696af837
shazmend <engshathaalfqih@gmail.com>
2023-09-11 13:57:23 +0300

    delete comment

--- Digital-Maintenance/src/Pages/FactoryReports.tsx
+++ Digital-Maintenance/src/Pages/FactoryReports.tsx
@@ -119,26 +119,6 @@ function FactoryReports() {
                           )}
                         </div>
                       </td>
-                      {/* <td className="px-4 py-3 text-sm border w-1/4 h-1/4">
-                          <a className="flex items-center justify-between hover:text-light-blue" href={`/FactoryReports/${task.id}`}>
-                            {" "}
-                            Show Report
-                            <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          className="w-4 h-3"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                          />
-                        </svg>
-                          </a>
-                        </td> */}
                       <td className="px-4 py-3 text-sm border w-1/4 h-1/4">
                         <a
                           className="flex items-center justify-between hover:text-light-blue cursor-pointer"
